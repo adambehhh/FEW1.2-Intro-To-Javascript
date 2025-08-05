c67fa401ce07f9331fded53518f195608424c811
adambehhh <behhh888@gmail.com>
2025-08-05 16:34:26 +0800

    JavaScript Fundamentals Exercises

---
+++ javascript exercise/exercise1.js
@@ -0,0 +1,7 @@
+let fullName = "Adam Beh";
+let age = 20;
+let isStudent = true;
+
+let greeting = "Hello, my name is " + fullName + ", I am " + age + " years old, and it is " + isStudent + " that I am a student.";
+
+console.log(greeting); 
